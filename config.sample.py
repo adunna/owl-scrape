f1fb99da4a21c23449e7bfe8adfba326fef2189f
adunna <adunna@umass.edu>
2019-04-12 02:07:21 -0400

    remove sample config

--- config.sample.py
+++
@@ -1,9 +0,0 @@
-# COPY ME INTO 'config.py'
-# CHANGE MY OAUTH_TOKEN TO THE RESPONSE FROM:
-# streamlink --twitch-oauth-authenticate
-# Quality options are "audio_only", "360p", "480p", "720p", and "best"
-# (best = 1080p60)
-
-CLIENT_ID = 'jzkbprff40iqj646a697cyrvl0zt2m6'
-OAUTH_TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-QUALITY = "best"
